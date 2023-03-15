4eee08f8fb78635a7bbd485dcb7c3559e240fe6b
pedrehh <silva.pedro3@aluno.ifsp.edu.br>
2023-03-15 10:26:28 -0300

    Adicionar arquivos de projeto.

---
+++ atv1pr2/Program.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv1pr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int metro;
+            //float cm, ml;
+
+            //Console.WriteLine("Digite o valor do metro: ");
+            //metro = int.Parse(Console.ReadLine());  
+
+
+            //cm = metro * 100;
+            //ml = metro * 1000;
+
+            //Console.WriteLine("O valor em centimetros é: " + cm);
+            //Console.WriteLine("O valor em milimetro é: " + ml);
+
+
+            //Console.ReadKey();  
+
+
+
+
+
+
+            //float f, Cfloat, Cint;
+            //int fint;
+
+            //Console.WriteLine("Digite o valor do grau em Fahrenheit: ");
+            //f = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite o valor do grau em Fahrenheit: ");
+            //fint = int.Parse(Console.ReadLine()); 
+            //Cfloat = (float)(f - 32.0f) * (5.0f / 9.0f);
+            //Cint = (fint - 32) * (5 / 9);
+
+
+            //Console.WriteLine("O valor do grau em Celsius  é: " + Cfloat);
+            //Console.WriteLine("O valor do grau em Celsius  é: " + Cint);
+
+
+            //Console.ReadKey();  
+
+
+
+
+
+
+
+            //Atividade 3
+
+
+            //float peso, altura, IMC;
+
+            //Console.WriteLine("Digite o valor do seu peso: ");
+            //peso = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite o valor da sua altura: ");
+            //altura = float.Parse(Console.ReadLine()); 
+
+
+            //IMC = peso / (altura * altura);
+
+            //Console.WriteLine("O valor do seu Índice de Massa Corporal é: " + IMC);
+
+
+
+            //Console.ReadKey();
+
+
+
+
+
+
+
+            //Atividade 4
+
+            //float resultado, n1, n2, n3, p1, p2, p3;
+
+            //Console.WriteLine("Digite um número: ");
+            //n1 = float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite um número: ");
+            //n2 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um número: ");
+            //n3 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um peso: ");
+            //p1 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um peso: ");
+            //p2 = float.Parse(Console.ReadLine());  
+
+            //Console.WriteLine("Digite um peso: ");
+            //p3 = float.Parse(Console.ReadLine());  
+
+            //resultado = (n1 + n2 + n3 + p1 + p2 + p3);
+
+
+            //Console.WriteLine("O valor da média é: " + resultado / 6.0f);
+
+
+            //Console.ReadKey();  
+
+
+
+
+
+
+            //Atividade 5
+
+            // float velo, temp, Aceleracao;
+
+            // Console.WriteLine("Digite o valor da velocidade : ");
+            // velo = float.Parse(Console.ReadLine());  
+
+            // Console.WriteLine("Digite o valor do tempo: ");
+            // temp = float.Parse(Console.ReadLine());  
+
+
+            // Aceleracao = velo / temp;
+
+            // Console.WriteLine("A aceleração escalar média é: " + Aceleracao);
+
+
+
+            // Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            // Atividade 6
+
+            // float salario, porcent, nv;
+
+            // Console.WriteLine("Digite o valor do seu salario : ");
+            // salario = float.Parse(Console.ReadLine());  
+
+            // porcent = (salario * 25) / 100;
+            // nv = salario + porcent;
+
+            // Console.WriteLine("O novo salario é: " + nv);
+            // Console.ReadKey();  
+
+
+
+
+
+
+
+
+
+            // Atividade 7
+
+            // float dolar, real, resultado;
+
+            // Console.WriteLine("Digite o valor em reais:  ");
+            // real = float.Parse(Console.ReadLine());  
+
+            // Console.WriteLine("Digite a cotação do dólar:  ");
+
+            // dolar = float.Parse(Console.ReadLine());
+
+            // resultado = real / dolar;
+
+            // Console.WriteLine("O valor em dolares é:  " + resultado);
+
+
+
+
+
+
+
+
+
+
+            // Atividade 8
+
+            //int num, resultado;
+
+            // Console.WriteLine("Digite um número: ");
+            // num = int.Parse(Console.ReadLine());
+
+            // resultado = (num * 3 + 1) + (num * 2 - 1);
+
+            // Console.WriteLine("O valor final é " + resultado);
+
+            // Console.ReadKey();
+
+
+
+
+
+
+
+
+            // Atividade 9
+
+            //int num, milhar, centena, dezena, unidade;
+
+            // Console.WriteLine("Digite um número inteiro de 4 digitos: ");
+            // num = int.Parse(Console.ReadLine());
+
+
+
+
+            // milhar = num / 1000;
+            // centena = (num / 100) % 10;
+            // dezena = (num / 10) % 10;
+            // unidade = num % 10;
+
+            // Console.WriteLine(milhar);
+            // Console.WriteLine(centena);
+            // Console.WriteLine(dezena);
+            // Console.WriteLine(unidade);
+
+
+            // Console.ReadKey();
+
+
+
+
+
+
+
+
+
+            // Atividade 10
+
+            //float inicio = 1000, dia1, dia2, dia3;
+
+            // inicio = inicio - (inicio * 3.4f) / 100;
+            // inicio = inicio + ((inicio * 2.8f) / 100);
+            // inicio = inicio + ((inicio * 14f) / 100);
+            // inicio = inicio - ((inicio * 8, 5f) / 100);
+
+
+            // Console.WriteLine(inicio);
+
+            // Console.ReadKey();
+
+
+
+
+
+
+
+
+            // Atividade 11
+
+            //  float n, cubo, quad;
+
+            // Console.WriteLine("Informe um numero ");
+            // n = float.Parse(Console.ReadLine());
+
+            // quad = n * n;
+            // cubo = n * n * n;
+
+            // Console.WriteLine("Seu número ao quadrado é:  " + quad);
+            // Console.WriteLine("Seu número ao quadrado é:  " + cubo);
+
+            // Console.ReadKey();
+
+
+
+
+
+
+
+            // Atividade 12
+
+            //float Altura, phomem, pmulher;
+
+            // Console.WriteLine("Informe sua altura:  ");
+            // Altura = float.Parse(Console.ReadLine());
+
+            // phomem = (72.7f * Altura) - 58f;
+            // pmulher = (62.1f * Altura) - 44.7f;
+
+            // Console.WriteLine("Para um homem de altura:  " + Altura + " o peso ideal é:  " + phomem);
+            // Console.WriteLine("Para uma mulher de altura:  " + Altura + " o peso ideal é:  " + pmulher);
+
+            // Console.ReadKey();
+
+
+
+
+
+            Atividade 13
+
+                GitHub é uma plataforma para gerenciar seu código e criar um ambiente de colaboração entre devs, utilizando o Git como sistema de controle.
+
+             Git
+             Estes sistemas de controle possuem a função de registrar quaisquer alterações feitas em cima de um código, armazenando essas informações e permitindo que, caso seja necessário, um(a) programador(a) possa regredir a versões anteriores de uma aplicação de modo simples e rápido.
+
+
+
+
+             Exemplos de Git
+
+             Criar um novo banco e trocar
+             git checkout - b bug - 456
+
+             Cria um stash
+             Git Stash
+
+             Iniciar pesquisa binária
+             git bisect start
+
+             Comitar um arquivo
+             git commit meu_arquivo.txt
+
+             Definir e - mail
+             git config --global user.email leonardo@software - ltda.com.br
+
+             Adiciona arquivos
+             git add .	
+
+             Remove arquivo
+             git rm meu_arquivo.txt
+
+             Exibir histórico
+             git log
+            Listar configurações
+             git config--lista
+
+             Criar um novo repositório
+             git init
+
+
+
+
+
+
+
+        }
+    }
+}
